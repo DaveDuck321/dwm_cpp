56821ae78e725a1c8ae811b8928d1c96fe1763b3
Tom Grant <thomas.grant.duck321@gmail.com>
2021-10-20 16:35:49 +0100

    Task: Format source files, add .gitignore

--- util.cpp
+++ util.cpp
@@ -6,20 +6,19 @@
 
 #include "util.hpp"
 
-void
-die(const char *fmt, ...) {
-	va_list ap;
+void die(const char* fmt, ...) {
+    va_list ap;
 
-	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
-	va_end(ap);
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
 
-	if (fmt[0] && fmt[strlen(fmt)-1] == ':') {
-		fputc(' ', stderr);
-		perror(NULL);
-	} else {
-		fputc('\n', stderr);
-	}
+    if (fmt[0] && fmt[strlen(fmt) - 1] == ':') {
+        fputc(' ', stderr);
+        perror(NULL);
+    } else {
+        fputc('\n', stderr);
+    }
 
-	exit(1);
+    exit(1);
 }
--- util.hpp
+++ util.hpp
@@ -1,19 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
-#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-#define MIN(A, B)               ((A) < (B) ? (A) : (B))
-#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+#define MAX(A, B) ((A) > (B) ? (A) : (B))
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
+#define BETWEEN(X, A, B) ((A) <= (X) && (X) <= (B))
 
-void die(const char *fmt, ...);
+void die(const char* fmt, ...);
 
+template <typename T> T* ecalloc(size_t nmemb) {
+    if (auto pointer = static_cast<T*>(calloc(nmemb, sizeof(T))); pointer) {
+        return pointer;
+    }
 
-template <typename T>
-T* ecalloc(size_t nmemb)
-{
-	if (auto pointer = static_cast<T*>( calloc(nmemb, sizeof(T))); pointer) {
-		return pointer;
-	}
-
-	die("calloc:");
+    die("calloc:");
     return nullptr;
 }
