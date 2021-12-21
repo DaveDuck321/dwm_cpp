DaveDuck321/dwm_cpp

commit 4c8c2087bed755c16098593f0aa5e1baae074607

Fix: regression to unicode rendering when correct font is unavailable
If no font can be found or generated to render a character, the first byte
of the character is now rendered using the default font. DWM has a similar
default behaviour -- this prevents illegal characters breaking the UI.

drw.cpp
@@ -74,23 +74,22 @@ std::string_view
 getContiguousCharactersWithRenderer(const DisplayFont* renderingFont,
                                     const std::vector<DisplayFont>& fonts,
                                     const std::string_view text) {
-
-    long utf8Codepoint;
     size_t utf8StringLength = 0;
-
     while (utf8StringLength < text.size()) {
+        long utf8Codepoint;
 
-        const auto utf8CharLength = utf8decode(
-            text.substr(utf8StringLength).data(), &utf8Codepoint, UTF_SIZ);
+        const auto suffix = text.substr(utf8StringLength);
+        const auto utf8CharLength =
+            utf8decode(suffix.data(), &utf8Codepoint, UTF_SIZ);
 
-        if (renderingFont != getFirstFontWithSymbol(fonts, utf8Codepoint)) {
+        if (renderingFont != getFirstFontWithSymbol(fonts, utf8Codepoint))
             break;
-        }
 
         utf8StringLength += utf8CharLength;
     }
 
-    return text.substr(0, utf8StringLength);
+    // Hack: from the original dwm -- always render a character
+    return text.substr(0, std::max(utf8StringLength, size_t{1}));
 }
 
 std::string_view cropTextToExtent(const DisplayFont& renderingFont,
@@ -121,9 +120,7 @@ CursorFont::~CursorFont() {
     }
 }
 
-Cursor CursorFont::getXCursor() const {
-    return *fCursor;
-}
+Cursor CursorFont::getXCursor() const { return *fCursor; }
 
 XColorScheme::XColorScheme(Display* display, const int screen,
                            const ColorScheme& scheme) {
@@ -207,15 +204,14 @@ bool DisplayFont::doesCodepointExistInFont(long utf8Codepoint) const {
     return XftCharExists(fDisplay, fXfont, utf8Codepoint);
 }
 
-DisplayFont
+std::optional<DisplayFont>
 DisplayFont::generateDerivedFontWithCodepoint(const int screen,
                                               const long utf8Codepoint) const {
     auto* fcCharSet = FcCharSetCreate();
     FcCharSetAddChar(fcCharSet, utf8Codepoint);
 
-    if (!fPattern) {
-        die("the first font in the cache must be loaded from a font string.");
-    }
+    if (!fPattern)
+        die("First font in the cache must be loaded from a font string.");
 
     auto* fcPattern = FcPatternDuplicate(fPattern);
     FcPatternAddCharSet(fcPattern, FC_CHARSET, fcCharSet);
@@ -231,16 +227,16 @@ DisplayFont::generateDerivedFontWithCodepoint(const int screen,
     FcCharSetDestroy(fcCharSet);
     FcPatternDestroy(fcPattern);
 
-    if (!match) {
-        die("TODO: figure out what should happen here");
+    if (!match)
+        die("Match fail: TODO: figure out what should happen here");
+
+    if (DisplayFont newFont{fDisplay, match};
+        newFont.doesCodepointExistInFont(utf8Codepoint)) {
+        return newFont;
     }
 
-    DisplayFont newFont{fDisplay, match};
-    if (!newFont.doesCodepointExistInFont(utf8Codepoint)) {
-        die("TODO: figure out what should happen here");
-    }
-
-    return newFont;
+    fprintf(stderr, "Codepoint doesn't exist: reverting to default font\n");
+    return std::nullopt;
 }
 
 uint DisplayFont::getHeight() const { return fXfont->ascent + fXfont->descent; }
@@ -353,9 +349,13 @@ int Drw::renderText(int x, const int y, uint w, uint h, const uint lpad,
         if (!renderingFont) {
             // Make a new font to render this character
             // NOTE: pointer into vector: don't mutate fFonts past this point
-            renderingFont =
-                &fFonts.emplace_back(fFonts[0].generateDerivedFontWithCodepoint(
-                    fScreen, utf8Codepoint));
+            auto newFont = fFonts[0].generateDerivedFontWithCodepoint(
+                fScreen, utf8Codepoint);
+            if (newFont) {
+                renderingFont = &fFonts.emplace_back(std::move(*newFont));
+            } else {
+                renderingFont = &(*fFonts.begin());
+            }
         }
 
         const auto textToRender =

drw.hpp
@@ -50,8 +50,8 @@ class DisplayFont {
 
     bool doesCodepointExistInFont(long utf8Codepoint) const;
 
-    DisplayFont generateDerivedFontWithCodepoint(int screen,
-                                                 long utf8Codepoint) const;
+    std::optional<DisplayFont>
+    generateDerivedFontWithCodepoint(int screen, long utf8Codepoint) const;
 
     uint getHeight() const;
     uint getTextExtent(std::string_view) const;

commit b056112e720717f8f6be68d3e9f4dda4edcbf4a8

Fix: resolve crash when rendering missing emoji
4c8c208 introduced a rendering regression: unicode boundaries are not
respected when rendering missing characters. This causes a crash in X11
when the second byte of a character is also valid unicode.

Additionally, a use-after-free error sometimes occurred when new fonts are
created to render specific missing characters. This is resolved by no-longer
taking ownership of the patterns produced by the parent font.

drw.cpp
@@ -82,14 +82,14 @@ getContiguousCharactersWithRenderer(const DisplayFont* renderingFont,
         const auto utf8CharLength =
             utf8decode(suffix.data(), &utf8Codepoint, UTF_SIZ);
 
-        if (renderingFont != getFirstFontWithSymbol(fonts, utf8Codepoint))
+        if (utf8StringLength > 0 &&
+            renderingFont != getFirstFontWithSymbol(fonts, utf8Codepoint)) {
             break;
+        }
 
         utf8StringLength += utf8CharLength;
     }
-
-    // Hack: from the original dwm -- always render a character
-    return text.substr(0, std::max(utf8StringLength, size_t{1}));
+    return text.substr(0, utf8StringLength);
 }
 
 std::string_view cropTextToExtent(const DisplayFont& renderingFont,
@@ -139,22 +139,7 @@ XColorScheme::XColorScheme(Display* display, const int screen,
     }
 }
 
-void DisplayFont::dieIfFontInvalid() const {
-    // This isn't the original behaviour, should just throw to prevent Font
-    // construction In DWM they just returned a null pointer instead
-
-    /* Using the pattern found at font->xfont->pattern does not yield the
-     * same substitution results as using the pattern returned by
-     * FcNameParse; using the latter results in the desired fallback
-     * behaviour whereas the former just results in missing-character
-     * rectangles being drawn, at least with some fonts. */
-    if (!fXfont) {
-        die("cannot load font from name:");
-    }
-    if (!fPattern) {
-        die("cannot parse font pattern:");
-    }
-
+void DisplayFont::dieIfFontIsColored() const {
     /* Do not allow using color fonts. This is a workaround for a BadLength
      * error from Xft with color glyphs. Modelled on the Xterm workaround. See
      * https://bugzilla.redhat.com/show_bug.cgi?id=1498269
@@ -171,9 +156,12 @@ void DisplayFont::dieIfFontInvalid() const {
 
 DisplayFont::DisplayFont(Display* display, FcPattern* pattern)
     : fDisplay{display}, fXfont{XftFontOpenPattern(display, pattern)},
-      fPattern{pattern} {
+      fPattern{nullptr} {
 
-    dieIfFontInvalid();
+    if (!fXfont)
+        die("error, cannot load font from pattern:");
+
+    dieIfFontIsColored();
 }
 
 DisplayFont::DisplayFont(Display* display, const int screen,
@@ -181,7 +169,17 @@ DisplayFont::DisplayFont(Display* display, const int screen,
     : fDisplay{display}, fXfont{XftFontOpenName(display, screen, fontname)},
       fPattern{FcNameParse((FcChar8*)fontname)} {
 
-    dieIfFontInvalid();
+    /* Using the pattern found at font->xfont->pattern does not yield the
+     * same substitution results as using the pattern returned by
+     * FcNameParse; using the latter results in the desired fallback
+     * behaviour whereas the former just results in missing-character
+     * rectangles being drawn, at least with some fonts. */
+    if (!fXfont)
+        die("cannot load font from name:");
+    if (!fPattern)
+        die("error, cannot parse font name to pattern:");
+
+    dieIfFontIsColored();
 }
 
 DisplayFont::DisplayFont(DisplayFont&& other)
@@ -348,7 +346,7 @@ int Drw::renderText(int x, const int y, uint w, uint h, const uint lpad,
 
         if (!renderingFont) {
             // Make a new font to render this character
-            // NOTE: pointer into vector: don't mutate fFonts past this point
+            // NOTE: don't mutate fFonts past this point
             auto newFont = fFonts[0].generateDerivedFontWithCodepoint(
                 fScreen, utf8Codepoint);
             if (newFont) {
@@ -366,7 +364,7 @@ int Drw::renderText(int x, const int y, uint w, uint h, const uint lpad,
             cropTextToExtent(*renderingFont, textToRender, w);
 
         if (!croppedTextToRender.empty()) {
-            // TODO: render elipsis here if textToRender != croppedTextToRender
+            // TODO: render elipsis if textToRender != croppedTextToRender
 
             if (shouldRender) {
                 const auto ty = y + (h - renderingFont->getHeight()) / 2 +

drw.hpp
@@ -58,7 +58,7 @@ class DisplayFont {
     XftFont* getXFont() const;
 
   private:
-    void dieIfFontInvalid() const;
+    void dieIfFontIsColored() const;
 
     Display* fDisplay;
     XftFont* fXfont;